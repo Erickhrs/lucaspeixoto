ac0c5e9bb8b26ba96559a131b6ec7e1aba8ab948
Erickhrs <erickhenriquee4444@gmail.com>
2024-08-25 13:19:33 -0300

    NEW BASE

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lucas Peixoto</title>
+    <link rel="stylesheet" href="./styles.php/global.css">
+    <link rel="stylesheet" href="./styles.php/index.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <nav>
+        <div id="logo">
+            <h1>LUCAS PEIXOTO</h1>
+        </div>
+        <div id="options">
+            <a href="" class="active">work</a>
+            <a href="">about</a>
+        </div>
+        <div id="social">
+            <a href=""><i class='bx bxl-instagram'></i></a>
+            <a href=""><i class='bx bxl-linkedin'></i></a>
+            <a href=""><i class='bx bx-envelope'></i></a>
+        </div>
+    </nav>
+    <div class="projects_container" style="">
+        <div class="project">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Nokota_Horses_cropped.jpg" alt="">
+            <p class="project_title">TRABALHO 1</p>
+        </div>
+        <div class="project">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Nokota_Horses_cropped.jpg" alt="">
+            <p class="project_title">TRABALHO 2</p>
+        </div>
+        <div class="project">
+            <img src="https://img.freepik.com/fotos-gratis/tiro-vertical-de-um-corpo-de-agua-com-o-ceu-rosa-durante-o-por-do-sol-perfeito-para-um-papel-de-parede_181624-5246.jpg"
+                alt="">
+            <p class="project_title">TRABALHO 3</p>
+        </div>
+
+        <div class="project">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Nokota_Horses_cropped.jpg" alt="">
+        </div>
+        <div class="project">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Nokota_Horses_cropped.jpg" alt="">
+        </div>
+        <div class="project">
+            <img src="https://img.freepik.com/fotos-gratis/tiro-vertical-de-um-corpo-de-agua-com-o-ceu-rosa-durante-o-por-do-sol-perfeito-para-um-papel-de-parede_181624-5246.jpg"
+                alt="">
+            <p class="project_title">TRABALHO 3</p>
+        </div>
+        <div class="project">
+            <img src="https://drive.google.com/file/d/bc1qf92drq0wwm8w7rnw9d8p4wjvaut2csdd5sg2cx/view" alt="">
+        </div>
+        <div class="project">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Nokota_Horses_cropped.jpg" alt="">
+        </div>
+        <div class="project">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Nokota_Horses_cropped.jpg" alt="">
+        </div>
+    </div>
+
+
+    <footer>
+        <a href=""><i class='bx bxl-instagram'></i></a>
+        <a href=""><i class='bx bxl-linkedin'></i></a>
+        <a href=""><i class='bx bx-envelope'></i></a>
+    </footer>
+</body>
+
+</html>
